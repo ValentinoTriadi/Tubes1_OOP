91ece83802678dad5a1ca95849a80dd3d66e465e
M Fauzan Azhim <101320828+fauzanazz@users.noreply.github.com>
2024-04-01 22:59:46 +0700

    feat (comment)

--- src/class/container/ladang/ladang.hpp
+++ src/class/container/ladang/ladang.hpp
@@ -7,16 +7,64 @@
 
 class Ladang : public Container {
     public:
+            /**
+             * @brief Default constructor for the Ladang class.
+             */
             Ladang();
+
+            /**
+             * @brief Constructs a Ladang object with the specified number of rows and columns.
+             *
+             * @param row The number of rows in the ladang.
+             * @param col The number of columns in the ladang.
+             */
             Ladang(int row, int col);
+
+            /**
+             * @brief Destructor for the Ladang class.
+             *
+             * This destructor is responsible for cleaning up any resources
+             * allocated by the Ladang class.
+             */
             ~Ladang();
+
+            /**
+             * @brief Tanam tanaman pada ladang
+             */
             void showPlant();
 
+
+            /**
+             * @brief Panen tanaman pada ladang
+             */
             std::map<std::string, int> getHarvest();
 
+
+            /**
+             * @brief Set the item at the specified position in the ladang.
+             *
+             * This function sets the item at the specified position (i, j) in the ladang.
+             * The item is represented by a pointer to an object of type Item.
+             *
+             * @param i The row index of the position.
+             * @param j The column index of the position.
+             * @param item A pointer to the item to be set.
+             */
             friend std::ostream& operator<<(std::ostream& os, const Ladang& ladang);
 
         private:
+            /**
+             * @brief Mencetak baris dari ladang
+             * @param os
+             * @param i
+             * @return
+             */
             std::ostream& printRow(std::ostream& os, int i) const;
+
+            /**
+             * @brief Mencetak header dari ladang
+             * @param os
+             * @return
+             */
             bool isReadyToHarvest(Item* item) const;
 };
--- src/class/container/peternakan/peternakan.hpp
+++ src/class/container/peternakan/peternakan.hpp
@@ -7,13 +7,47 @@
 
 class Peternakan : public Container {
     public:
+            /**
+             * @brief Default constructor for the Peternakan class.
+             */
             Peternakan();
+
+            /**
+             * @brief Constructs a Peternakan object with the specified number of rows and columns.
+             *
+             * @param row The number of rows in the peternakan.
+             * @param col The number of columns in the peternakan.
+             */
             Peternakan(int row, int col);
+
+            /**
+             * @brief Destructor for the Peternakan class.
+             *
+             * This destructor is responsible for cleaning up any resources
+             * allocated by the Peternakan class.
+             */
             ~Peternakan();
+
+            /**
+             * @brief Show animal in the peternakan
+             */
             void showAnimal();
 
+            /**
+             * @brief Get the animal in the peternakan
+             */
             std::map<std::string, int> getHarvest();
 
+            /**
+             * @brief Set the item at the specified position in the peternakan.
+             *
+             * This function sets the item at the specified position (i, j) in the peternakan.
+             * The item is represented by a pointer to an object of type Item.
+             *
+             * @param i The row index of the position.
+             * @param j The column index of the position.
+             * @param item A pointer to the item to be set.
+             */
             friend std::ostream& operator<<(std::ostream& os, const Peternakan& ladang);
 
         private:
